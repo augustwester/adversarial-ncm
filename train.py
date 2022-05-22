1a2253f061d9ee52ae250c7b1b4761ada7b54062
August Wester <august.wester@gmail.com>
2022-05-22 14:47:00 +0200

    Add LR scheduler and set t=0.4

--- train.py
+++ train.py
@@ -12,8 +12,7 @@ def train(X: Tensor,
           d: Discriminator,
           batch_size: int,
           num_epochs: int,
-          num_pretraining_epochs=100,
-          threshold=0.5):
+          threshold=0.4):
     """
     Trains the NCM and matrix of edge beliefs represented by the generator.
 
@@ -23,7 +22,6 @@ def train(X: Tensor,
         d: Discriminator network
         batch_size: The batch size used during training
         num_epochs: Number of epochs to train for (excluding pretraining)
-        num_pretraining_epochs: Number of epochs for pretraining the model
         threshold: The threshold value by which an edge is included in the final prediction
 
     Returns:
@@ -31,16 +29,18 @@ def train(X: Tensor,
         generator and discriminator, and a list of edge beliefs recorded
         throughout training.
     """
-
-    g_opt = RMSprop(g.parameters(), lr=1e-3)
+    e_opt = RMSprop(g.edge_beliefs.parameters(), lr=5e-3)
+    g_opt = RMSprop(g.ncm.parameters(), lr=1e-3)
     d_opt = RMSprop(d.parameters(), lr=1e-3)
     bce = nn.BCELoss()
+    scheduler = torch.optim.lr_scheduler.LinearLR(e_opt, start_factor=0.1, total_iters=num_epochs)
 
     N = g.num_nodes
     P_hist = [[] for _ in range(N**2)]
     g_losses, d_losses = [], []
 
-    def run_epoch(X, lambda_e, lambda_dag, do=None, update_edge_beliefs=True):
+    def run_epoch(X, lambda_e, lambda_dag, g_opt, d_opt, do=None, update_ncm=True, update_edge_beliefs=True):
+        X = X[torch.randperm(len(X))]
         num_batches = len(X) // batch_size
         batch_sizes = [batch_size for _ in range(num_batches)]
 
@@ -58,8 +58,6 @@ def train(X: Tensor,
             m_exp = torch.trace(torch.matrix_exp(g.edge_beliefs.edge_beliefs)) - N
             g_loss = bce(preds, y_real) + lambda_e * A.mean(0).sum() + lambda_dag * m_exp
             g_loss.backward()
-            if not update_edge_beliefs:
-                g.edge_beliefs.P.grad = torch.zeros_like(g.edge_beliefs.P)
             g_opt.step()
 
             # train discriminator
@@ -77,26 +75,25 @@ def train(X: Tensor,
             d_loss.backward()
             d_opt.step()
 
-    print("Pretraining...")
-    X_data = X[X[:,-1] == 1]
-    for _ in (_ := tqdm(range(num_pretraining_epochs))):
-        run_epoch(X_data, lambda_e=0, lambda_dag=0, do=-1, update_edge_beliefs=False)
-
-    print("Training...")
-    for i in (pbar := tqdm(range(num_epochs))):
+    for i in (_ := tqdm(range(num_epochs))):
         progress = i / num_epochs
         lambda_e = max(0.1 - (progress * 0.1), 0.01)
-        lambda_dag = progress
-        run_epoch(X, lambda_e, lambda_dag, do=None)
+        lambda_dag = 0.1*progress
+
+        run_epoch(X, 0, 0, g_opt, d_opt, do=None, update_edge_beliefs=False)
+        run_epoch(X, lambda_e, lambda_dag, e_opt, d_opt, do=None, update_ncm=False)
+        scheduler.step()
 
         for r in range(N):
             for c in range(N):
                 belief = torch.sigmoid(g.edge_beliefs.P.T[r,c]).item()
                 P_hist[r*N+c].append(belief)
 
-        if i % 10 == 0:
-            eb = g.edge_beliefs.edge_beliefs.detach().numpy()
-            print(np.around(eb, decimals=3))
+        A = g.edge_beliefs.edge_beliefs
+        num_pos = len(A[A > 0.4])
+        num_neg = len(A[A < 0.001])
+        if num_neg > N and num_pos+num_neg == N**2:
+            break
 
     P = torch.sigmoid(g.edge_beliefs.P.T).detach().numpy()
     A_hat = P > threshold
