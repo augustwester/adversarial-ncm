d9406d2b155be017b783fe24c85b35f6a5545334
August Wester <august.wester@gmail.com>
2022-05-30 21:55:05 +0200

    Add extra params to eval script

--- eval.py
+++ eval.py
@@ -12,7 +12,7 @@ from discriminator import Discriminator
 from train import train
 from run import compute_shd, run, save_loss_plot, save_samples_plot, save_txt
 
-def evaluate(graph_type: GraphType, fn_type: FnType, num_nodes: int, batch_size: int):
+def evaluate(graph_type: GraphType, fn_type: FnType, num_nodes: int, num_epochs: int, batch_size: int):
     """
     Evaluate the proposed method 5 times on data from SCMs obeying the
     specified parameters.
@@ -36,13 +36,13 @@ def evaluate(graph_type: GraphType, fn_type: FnType, num_nodes: int, batch_size:
                     fn_type,
                     num_nodes,
                     batch_size=batch_size,
-                    num_epochs=None)
-        g, scm, shd, g_losses, d_losses, p_hist = stats
+                    num_epochs=num_epochs,
+                    verbose=False)
+        g, scm, shd, g_losses, d_losses, p_hist, X = stats
         shds.append(shd)
         output_dir = "/".join([container_dir, str(i+1)]) + "/"
         os.makedirs(output_dir)
-        graph_name = graph_type.name.lower() + str(num_nodes)
-        save_loss_plot(graph_name, g_losses, d_losses, p_hist, output_dir)
+        save_loss_plot(graph_type, g_losses, d_losses, p_hist, output_dir)
         txt = open(output_dir + "/info.txt", "w")
         txt.write(
             f"""
@@ -67,11 +67,13 @@ def evaluate(graph_type: GraphType, fn_type: FnType, num_nodes: int, batch_size:
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--batch_size", type=int, default=256)
+    parser.add_argument("--num_epochs", type=int, default=None)
     parser.add_argument("--type", type=str, required=True)
 
     args = parser.parse_args()
     fn_type = FnType(args.type.lower())
     batch_size = args.batch_size
+    num_epochs = args.num_epochs
 
     graphs = [GraphType.CHAIN,
               GraphType.COLLIDER,
@@ -81,10 +83,13 @@ if __name__ == "__main__":
               GraphType.ER1,
               GraphType.ER2,
               GraphType.FULL]
-    #nodes = [4, 6, 8, 10]
-    nodes = [6,8,10]
+    nodes = [4, 6, 8, 10]
 
     for num_nodes in nodes:
-        ps = [Process(target=evaluate, args=(graph, fn_type, num_nodes, batch_size)) for graph in graphs]
+        ps = []
+        for graph in graphs:
+            if num_nodes < 3 and graph is GraphType.ER1: continue
+            if num_nodes < 5 and graph is GraphType.ER2: continue
+            ps.append(Process(target=evaluate, args=(graph, fn_type, num_nodes, num_epochs, batch_size)))
         for p in ps: p.start()
         for p in ps: p.join()
